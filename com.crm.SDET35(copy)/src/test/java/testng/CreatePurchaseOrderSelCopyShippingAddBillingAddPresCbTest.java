2f749523cb2f8993e96ba2801eb0016fc77d3144
SanskarShri <107914527+SanskarShri@users.noreply.github.com>
2022-06-22 15:52:00 +0530

    Update CreatePurchaseOrderSelCopyShippingAddBillingAddPresCbTest.java

--- com.crm.SDET35(copy)/src/test/java/testng/CreatePurchaseOrderSelCopyShippingAddBillingAddPresCbTest.java
+++ com.crm.SDET35(copy)/src/test/java/testng/CreatePurchaseOrderSelCopyShippingAddBillingAddPresCbTest.java
@@ -19,7 +19,7 @@ public class CreatePurchaseOrderSelCopyShippingAddBillingAddPresCbTest extends B
 	{
 		
 		
-		//TC-30 is the test case present in PurchaseOrder Assignmen
+		//TC-30 is the test case present in PurchaseOrder Assignment
 		
 		
 		//To get Random number
